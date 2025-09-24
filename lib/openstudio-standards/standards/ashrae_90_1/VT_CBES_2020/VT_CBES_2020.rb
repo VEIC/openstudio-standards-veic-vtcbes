cd1eedbf6e47abf6f7f0188d2d680ad477ff5de5
Matt Deslauriers <MDeslauriers@veic.org>
2025-09-24 16:20:25 -0400

    Revised space type file name and removed old file

--- lib/openstudio-standards/standards/ashrae_90_1/VT_CBES_2020/VT_CBES_2020.rb
+++ lib/openstudio-standards/standards/ashrae_90_1/VT_CBES_2020/VT_CBES_2020.rb
@@ -1,6 +1,6 @@
-# This class holds methods that apply ASHRAE 90.1-2016
+# This class holds methods that apply VT CBES 2020 specific measures where they deviate from 90.1-2016
 # to a given model.
-# @ref [References::ASHRAE9012016]
+# @ref [References::VT_CBES_2020]
 class VTCBES2020 < ASHRAE901
   register_standard 'VT_CBES_2020'
   attr_reader :template
