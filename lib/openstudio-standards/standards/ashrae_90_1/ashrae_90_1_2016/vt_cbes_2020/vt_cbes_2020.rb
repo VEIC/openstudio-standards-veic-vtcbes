77968d488335ba424fc7d3ebca8104878723e30e
Vince Caristo <vince.caristo@gmail.com>
2026-03-17 09:49:08 -0600

    add child class under 90_1_2016

---
+++ lib/openstudio-standards/standards/ashrae_90_1/ashrae_90_1_2016/vt_cbes_2020/vt_cbes_2020.rb
@@ -0,0 +1,20 @@
+# vt_ashrae_90_1_2016.rb
+class VTCBES2020 < ASHRAE9012016
+  register_standard 'VT-CBES-2020'
+  attr_reader :template
+
+  def initialize
+    super()
+    @template = 'VT-CBES-2020'
+    load_standards_database
+  end
+
+  # this loads all the JSONs in ./data
+  # see /home/vcaristo/projects/vt_cbes_child_class/openstudio-standards-veic-vtcbes/lib/openstudio-standards/standards/standard.rb
+  def load_standards_database(data_directories = [])
+    super([__dir__] + data_directories)
+  end
+end
+
+# we can define the class in separate files like this:
+# require_relative 'vt_ashrae_90_1_2016.AirLoopHVAC'  
